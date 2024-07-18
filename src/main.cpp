7f4772309d557bb0a6b1f31ea2b6245161480f9f
Tishan Kashmeera <tishan.21@cse.mrt.ac.lk>
2024-07-18 11:53:20 +0530

    Latest

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,8 @@
 #include <DHTesp.h>
 #include <WiFi.h>
 #include <time.h>
+#include <ESP32Servo.h> // Include the servo motor library
+#include <PubSubClient.h>
 
 // Definitions
 #define NTP_SERVER "pool.ntp.org"
@@ -20,16 +22,24 @@
 #define LED_1 15
 #define LED_2 2
 #define LED_3 4
-#define CANCEL 34
-#define UP 35
+#define CANCEL 14
+#define UP 27
 #define DOWN 32
 #define OK 33
 #define ALARM 25
 #define DHT 12
+#define LEFT_LDR_PIN 34
+#define RIGHT_LDR_PIN 35
+#define SERVO_PIN 5
 
 // Object Declarations
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 DHTesp dhtSensor;
+
+Servo servoMotor;
+
+WiFiClient espClient;
+PubSubClient mqttClient(espClient);
 
 // Variables
 int n_notes = 8;
@@ -55,6 +65,10 @@ int alarm_hours[] = {0, 0, 0};
 int alarm_minutes[] = {1, 2, 3};
 bool alarm_triggered[] = {false, false, false};
 
+float THETA_OFFSET[] = {30, 55, 60, 0};
+float GAMMA[] = {0.75, 0.5, 0.86, 0};
+int tablet = 0;
+
 volatile bool cancelButtonPressed = false;
 volatile bool okButtonPressed = false;
 
@@ -76,6 +90,20 @@ String time_zones[][2] =
     {{"-1200", "<-12>12"}, {"-1130", "<-1130>11:30"}, {"-1100", "<-11>11"}, {"-1030", "<-1030>10:30"}, {"-1000", "<-10>10"}, {"-0930", "<-0930>9:30"}, {"-0900", "<-09>9"}, {"-0830", "<-0830>8:30"}, {"-0800", "<-08>8"}, {"-0730", "<-0730>7:30"}, {"-0700", "<-07>7"}, {"-0630", "<-0630>6:30"}, {"-0600", "<-06>6"}, {"-0530", "<-0530>5:30"}, {"-0500", "<-05>5"}, {"-0430", "<-0430>4:30"}, {"-0400", "<-04>4"}, {"-0330", "<-0330>3:30"}, {"-0300", "<-03>3"}, {"-0230", "<-0230>2:30"}, {"-0200", "<-02>2"}, {"-0130", "<-0130>1:30"}, {"-0100", "<-01>1"}, {"-0030", "<-0030>0:30"}, {"+0000", "<+00>-0"}, {"+0030", "<+0030>-0:30"}, {"+0100", "<+01>-1"}, {"+0130", "<+0130>-1:30"}, {"+0200", "<+02>-2"}, {"+0230", "<+0230>-2:30"}, {"+0300", "<+03>-3"}, {"+0330", "<+0330>-3:30"}, {"+0400", "<+04>-4"}, {"+0430", "<+0430>-4:30"}, {"+0500", "<+05>-5"}, {"+0530", "<+0530>-5:30"}, {"+0600", "<+06>-6"}, {"+0630", "<+0630>-6:30"}, {"+0700", "<+07>-7"}, {"+0730", "<+0730>-7:30"}, {"+0800", "<+08>-8"}, {"+0830", "<+0830>-8:30"}, {"+0900", "<+09>-9"}, {"+0930", "<+0930>-9:30"}, {"+1000", "<+10>-10"}, {"+1030", "<+1030>-10:30"}, {"+1100", "<+11>-11"}, {"+1130", "<+1130>-11:30"}, {"+1200", "<+12>-12"}, {"+1230", "<+1230>-12:30"}, {"+1300", "<+13>-13"}, {"+1330", "<+1330>-13:30"}, {"+1400", "<+14>-14"}};
 int timezone = 25;
 
+int leftLDRValue = 0;
+int rightLDRValue = 0;
+float maxIntensity = 0.0;
+float l_LDR = 0.0;
+float R_LDR = 0.0;
+
+char tempAr[6];
+char humidAr[6];
+
+// LDR Characteristics
+const float GAMMA_LDR = 0.7;
+const float RL10 = 50;
+
+// Functions
 void clear_top_half() // clear top half of the oled display
 {
   display.fillRect(0, 0, display.width(), display.height() / 2, BLACK);
@@ -628,6 +656,9 @@ void check_temp(void) // function for DHT22 sensor data
   {
     digitalWrite(LED_2, LOW);
   }
+
+  String(data.temperature, 2).toCharArray(tempAr, 6);
+  String(data.humidity, 2).toCharArray(humidAr, 6);
 }
 
 void IRAM_ATTR okInterrupt()
@@ -666,6 +697,177 @@ void IRAM_ATTR alarmInterrupt()
   }
 }
 
+void measureLightIntensity()
+{
+  leftLDRValue = analogRead(LEFT_LDR_PIN);
+  rightLDRValue = analogRead(RIGHT_LDR_PIN);
+
+  // Calculate normalized LDR values
+  float l_LDR = 1.0 - ((leftLDRValue - 32.0) / (4063.0 - 32.0));
+  float r_LDR = 1.0 - ((rightLDRValue - 32.0) / (4063.0 - 32.0));
+
+  // Determine max intensity and publish
+  if (l_LDR > r_LDR)
+  {
+    maxIntensity = l_LDR;
+    mqttClient.publish("ADMIN-INTENSITY", String(maxIntensity).c_str());
+    mqttClient.publish("ADMIN-LDR", "LEFT");
+  }
+  else if (r_LDR > l_LDR)
+  {
+    maxIntensity = r_LDR;
+    mqttClient.publish("ADMIN-INTENSITY", String(maxIntensity).c_str());
+    mqttClient.publish("ADMIN-LDR", "RIGHT");
+  }
+  else
+  {
+    maxIntensity = l_LDR;
+    mqttClient.publish("ADMIN-INTENSITY", String(maxIntensity).c_str());
+    mqttClient.publish("ADMIN-LDR", "BOTH");
+  }
+}
+
+void adjustShadedWindow(float maxIntensity)
+{
+  // Calculate motor angle based on max intensity
+  float D = (l_LDR > R_LDR) ? 1.5 : 0.5;
+  float theta = min(float(THETA_OFFSET[tablet] * D + (180.0f - THETA_OFFSET[tablet]) * maxIntensity * GAMMA[tablet]), 180.0f);
+
+  // Move servo to calculated angle
+  servoMotor.write(theta);
+}
+
+void alarmOn(bool on)
+{
+  if (on)
+  {
+    alarm_enabled = true;
+    digitalWrite(LED_3, HIGH);
+  }
+  else
+  {
+    alarm_enabled = false;
+    digitalWrite(LED_3, LOW);
+  }
+}
+
+void medicineSelect(char medicine)
+{
+  if (medicine == 'A')
+  {
+    tablet = 0;
+  }
+  else if (medicine == 'B')
+  {
+    tablet = 1;
+  }
+  else if (medicine == 'C')
+  {
+    tablet = 2;
+  }
+  else if (medicine == 'D')
+  {
+    tablet = 3;
+  }
+}
+
+void setupWifi()
+{
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println("Wokwi-GUEST");
+  WiFi.begin("Wokwi-GUEST", "");
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(250);
+    display.clearDisplay();
+    print_line("Connecting to WiFi", 2, 0, 0); // (String, text_size, cursor_row, cursor_column)
+    Serial.print(".");
+  }
+
+  display.clearDisplay();
+  print_line("Connected to WiFi", 2, 0, 0);
+
+  Serial.println();
+  Serial.println("WiFi connected");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void setupMqtt()
+{
+  mqttClient.setServer("test.mosquitto.org", 1883);
+  mqttClient.setCallback(recieveCallback);
+}
+
+void recieveCallback(char *topic, byte *payload, unsigned int length)
+{
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("]");
+
+  char payloadCharAr[length];
+  for (int i = 0; i < length; i++)
+  {
+    // Serial.print((char)payload[i]);
+    payloadCharAr[i] = (char)payload[i];
+  }
+
+  if (strcmp(topic, "ADMIN-ALARM-ENABLER") == 0)
+  {
+    alarmOn(payloadCharAr[0] == '1');
+  }
+  else if (strcmp(topic, "ADMIN-MEDICINE") == 0)
+  {
+    medicineSelect(payloadCharAr[0]);
+    Serial.println(tablet);
+  }
+  else if (strcmp(topic, "ADMIN-MINANGLE") == 0)
+  {
+    if (tablet == 3)
+    {
+      String payloadValue = String(payloadCharAr);
+      float floatValue = atof(payloadValue.substring(0, payloadValue.indexOf('.') + 3).c_str());
+      THETA_OFFSET[3] = floatValue;
+      Serial.println(THETA_OFFSET[3]);
+    }
+  }
+  else if (strcmp(topic, "ADMIN-CONFACTOR") == 0)
+  {
+    if (tablet == 3)
+    {
+      String payloadValue = String(payloadCharAr);
+      float floatValue = atof(payloadValue.substring(0, payloadValue.indexOf('.') + 3).c_str());
+      GAMMA[3] = floatValue;
+      Serial.println(GAMMA[3]);
+    }
+  }
+}
+
+void connectToBroker()
+{
+  while (!mqttClient.connected())
+  {
+    Serial.print("Attempting MQTT connection...");
+    if (mqttClient.connect("ESP32-123456789"))
+    {
+      Serial.println("Connected");
+      // Subscribe functions
+      mqttClient.subscribe("ADMIN-ALARM-ENABLER");
+      mqttClient.subscribe("ADMIN-MEDICINE");
+      mqttClient.subscribe("ADMIN-MINANGLE");
+      mqttClient.subscribe("ADMIN-CONFACTOR");
+    }
+    else
+    {
+      Serial.println("failed ");
+      Serial.println(mqttClient.state());
+      delay(5000);
+    }
+  }
+}
+
 void setup()
 {
   Serial.begin(9600);
@@ -679,6 +881,10 @@ void setup()
   pinMode(DOWN, INPUT);
   pinMode(OK, INPUT);
   pinMode(ALARM, INPUT);
+  pinMode(LEFT_LDR_PIN, INPUT);
+  pinMode(RIGHT_LDR_PIN, INPUT);
+
+  servoMotor.attach(SERVO_PIN);
 
   dhtSensor.setup(DHT, DHTesp::DHT22);
 
@@ -697,16 +903,9 @@ void setup()
   print_line("Welcome to Medibox", 2, 0, 0); // (String, text_size, cursor_row, cursor_column)
   delay(3000);
 
-  WiFi.begin("Wokwi-GUEST", "", 6);
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    delay(250);
-    display.clearDisplay();
-    print_line("Connecting to WiFi", 2, 0, 0); // (String, text_size, cursor_row, cursor_column)
-  }
+  setupWifi();
 
-  display.clearDisplay();
-  print_line("Connected to WiFi", 2, 0, 0);
+  setupMqtt();
 
   configTime(UTC_OFFSET, UTC_OFFSET_DST, NTP_SERVER);
 
@@ -725,6 +924,12 @@ void setup()
 void loop()
 {
 
+  if (!mqttClient.connected())
+  {
+    connectToBroker();
+  }
+  mqttClient.loop();
+
   if (okButtonPressed)
   {
     delay(1000); // Add a small delay to avoid multiple interrupts
@@ -735,4 +940,10 @@ void loop()
 
   check_temp();
   update_time_with_check_alarm();
+
+  measureLightIntensity();
+  adjustShadedWindow(maxIntensity);
+
+  mqttClient.publish("ADMIN-TEMP", tempAr);
+  mqttClient.publish("ADMIN-HUMID", humidAr);
 }
